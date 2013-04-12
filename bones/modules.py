ce54c1cf10f146210f26583d1572ac2c632312fb
Simen Lybekk <simen@lybekk.no>
2013-04-12 23:27:45 +0200

    Change the trigger for the MC Server List module.

--- bones/modules.py
+++ bones/modules.py
@@ -1,9 +1,9 @@
 from bones.bot import Module
 
 class MinecraftServerList(Module):
-    def cmdMc(self, client, data, channel):
-        client.msg(channel, "Ouch!")
+    def cmdMc(self, client, args=None, channel=None, user=None, msg=None):
+        client.msg(channel, "%s: Wait wait, I'm charging my batteries!" % user.split("!")[0])
         
     triggerMap = {
-        "mc": cmdMc
+        "mcservers": cmdMc
     }
