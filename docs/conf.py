9eca83acb6d9f283319e83053ab187a00998a2b3
Simen Lybekk <simen@lybekk.no>
2014-11-10 16:01:42 +0100

    Update copyright notice for the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Bones IRC Bot'
-copyright = u'2013, 404\'d'
+copyright = u'2013 - 2014, 404\'d'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
