fffecca914ee9ac2b0035e727d92049202876f10
Simen Lybekk <simen@lybekk.no>
2014-12-10 08:35:46 +0100

    Add setuo.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Bones IRC Bot",
+    version="0.2.0-DEV",
+    description="""A \"barebones\" modular IRC Bot providing a framework for
+bot development using modules able to handle e.g. triggers and such.""",
+    author="Simen Lybekk",
+    author_email="simen4000+github-bones@gmail.com",
+    url="http://github.com/404d/Bones-IRCBot",
+    packages=["bones"],
+)
