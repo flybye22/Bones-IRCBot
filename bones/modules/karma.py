5221245a0880a0ccab99126bf080f052854790fe
Dillon Lareau <flybye22@gmail.com>
2015-07-28 17:33:47 -0400

    re-wrote karma.py to be more modular still needs some fixing

--- bones/modules/karma.py
+++ bones/modules/karma.py
@@ -9,45 +9,67 @@ NICK_RE = "[a-z_\-\[\]\\^{}|`][a-z0-9_\-\[\]\\^{}|`]{1,15}"
 
 class Karmabot(bones.bot.Module):
 
+    def getUserScore(self, user):
+        conn = sqlite3.connect('stats.db')
+        c = conn.cursor()
+        c.execute("SELECT count(*) FROM stats WHERE dest LIKE '%s'" % (user))
+        result = c.fetchone()[0]
+        conn.close()
+        return result
+
+    def getAllScores(self):
+        conn = sqlite3.connect('stats.db')
+        c = conn.cursor()
+        c.execute("SELECT dest, count(*) FROM stats GROUP BY dest ORDER BY count(*) DESC")
+        result = c.fetchall()
+        conn.close()
+        return result
+
+    def addKarmaEntry(self, source, dest, kind):
+        conn = sqlite3.connect('stats.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO stats VALUES (NULL, '%s', '%s', %d)" % (source, dest, kind))
+        conn.commit()
+        conn.close()
+
+    # searches, assumes username match will be the first group
+    def searchAndAdd(self, regex, event, kind):
+        s = re.search(regex, event.message)
+        if(s):
+            if(event.user.name == s.group(1)):
+                event.channel.msg("You can't karma yourself")
+            else:
+                self.addKarmaEntry(event.user.name, s.group(1), kind)
+            return True
+        return False
+
     # registers an event handler for whenever somebody speaks in channel
     @bones.event.handler(event=bones.event.ChannelMessageEvent)
     def publicMessage(self, event):
-        # matches start of line with colon or space and inline but without colon or space
-        s = re.search("(^(%s):? ?(?:\+\+|==)|(%s)(?:\+\+|==))" % (NICK_RE, NICK_RE), event.message)
-        conn = sqlite3.connect('stats.db')
-        c = conn.cursor()
-        if(s):
-            if(s.group(2)):
-                dest = s.group(2)
-            else:
-                dest = s.group(3)
-            if(event.user.name == dest):
-                event.channel.msg("You can't karma yourself")
-            else:
-                c.execute("INSERT INTO stats VALUES (NULL, '%s', '%s', 0)" % (event.user.nickname, dest))
-                conn.commit()
+        # various ++ and == rules
+        if(self.searchAndAdd("^(%s):? ?\+\+" % (NICK_RE), event, 0)):
+            pass
+        elif(self.searchAndAdd("(%s)\+\+" % (NICK_RE), event, 0)):
+            pass
+        elif(self.searchAndAdd("^== ?(%s)" % (NICK_RE), event, 1)):
+            pass
+        elif(self.searchAndAdd("==(%s)" % (NICK_RE), event, 1)):
+            pass
+        
         # matches start of line ".karma SOMENICK"
         # is slighlty vulnerable to SQL injection
         s = re.search("\A\.karma (%s)" % (NICK_RE), event.message)
-        if(s):    
-            c.execute("SELECT count(*) FROM stats WHERE dest LIKE '%s'" % (s.group(1)))
-            results = c.fetchone()[0]
-            event.channel.msg(
-                "%s has %d karma" % (s.group(1), results)
-            )
-        conn.close()
+        if(s):
+            val = self.getUserScore(s.group(1))
+            event.channel.msg("%s has %d karma" % (s.group(1), val))
         
     # registers an event handler for whenever somebody private messages the bot
     @bones.event.handler(event=bones.event.UserMessageEvent)
     def privMessage(self, event):
         if(event.message == "totals"):
-            conn = sqlite3.connect('stats.db')
-            c = conn.cursor()
-            c.execute("SELECT dest, count(*) FROM stats GROUP BY dest ORDER BY count(*) DESC")
             result = "List of Karmas:\r"
-            for person in c.fetchall():
+            for person in self.getAllScores():
                 result += str(person[0]) + ": " + str(person[1]) + "\r" 
-            conn.close()
             event.user.msg(result)
         else:
             event.user.msg("I don't support any private command besides 'totals' right now")
